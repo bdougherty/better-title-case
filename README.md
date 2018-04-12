b4ebe2179d848151c9b56cb65118de40d90e8bad
Brad Dougherty <me@brad.is>
2018-04-12 19:21:42 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,66 @@
+# better-title-case
+
+> Convert a string to title case based on the [Daring Fireball](https://daringfireball.net/2008/05/title_case) rules.
+
+## Rules
+
+- If the string is all-caps, it will be corrected
+- The following words are not capitalized by default: a, an, and, at, but, by, for, in, nor, of, on, or, so, the, to, up, yet, v, v., vs, and vs.
+- Words with capital letters other than the first are assumed to be capitalized properly and are skipped
+-  It also skips any word that looks like a file path, file name, or URL
+- The first and last word are always capitalized
+- Sub-strings (those that are within quotes or parens/braces) are capitalized according to the same rules
+
+
+## Installation
+
+```
+$ npm install --save better-title-case
+```
+
+
+## Usage
+
+```js
+const titleCase = require('better-title-case');
+console.log(titleCase('Nothing to Be Afraid of?'));
+// Nothing to Be Afraid Of?
+```
+
+
+## Advanced
+
+You can configure `better-title-case` to add your own excluded words to the default list, or to prevent the use of the default list by passing a `config` object as the second parameter.
+
+### excludedWords
+
+Type: `Array`<br>
+Default: `[]`
+
+Additional words to exclude from capitalization.
+
+```js
+titleCase('Nothing to be afraid of?', {
+    excludedWords: ['be']
+});
+// 'Nothing to be Afraid Of?'
+```
+
+### useDefaultExcludedWords
+
+Type: `Boolean`<br>
+Default: `true`
+
+Disable the usage of the default list of excluded words.
+
+```js
+titleCase('Nothing to be afraid of?', {
+	useDefaultExcludedWords: false
+});
+// 'Nothing To Be Afraid Of?'
+```
+
+
+## License
+
+MIT © [Brad Dougherty](https://brad.is)
---
+++ test.js
@@ -0,0 +1,54 @@
+import test from 'ava';
+import titleCase from '.';
+
+function convert(t, input, expected) {
+	t.is(titleCase(input), expected);
+}
+
+convert.title = (providedTitle, input) => `Properly capitalizes ”${input}“`;
+
+test(convert, undefined, '');
+test(convert, '', '');
+test(convert, `For step-by-step directions email someone@gmail.com`, `For Step-by-Step Directions Email someone@gmail.com`);
+test(convert, `2lmc Spool: 'Gruber on OmniFocus and Vapo(u)rware'`, `2lmc Spool: 'Gruber on OmniFocus and Vapo(u)rware'`);
+test(convert, `Have you read “The Lottery”?`, `Have You Read “The Lottery”?`);
+test(convert, `your hair[cut] looks (nice)`, `Your Hair[cut] Looks (Nice)`);
+test(convert, `People probably won't put http://foo.com/bar/ in titles`, `People Probably Won't Put http://foo.com/bar/ in Titles`);
+test(convert, `Scott Moritz and TheStreet.com’s million iPhone la‑la land`, `Scott Moritz and TheStreet.com’s Million iPhone La‑La Land`);
+test(convert, `BlackBerry vs. iPhone`, `BlackBerry vs. iPhone`);
+test(convert, `Notes and observations regarding Apple’s announcements from ‘The Beat Goes On’ special event`, `Notes and Observations Regarding Apple’s Announcements From ‘The Beat Goes On’ Special Event`);
+test(convert, `Read markdown_rules.txt to find out how _underscores around words_ will be interpretted`, `Read markdown_rules.txt to Find Out How _Underscores Around Words_ Will Be Interpretted`);
+test(convert, `Q&A with Steve Jobs: 'That's what happens in technology'`, `Q&A With Steve Jobs: 'That's What Happens in Technology'`);
+test(convert, `What is AT&T's problem?`, `What Is AT&T's Problem?`);
+test(convert, `Apple deal with AT&T falls through`, `Apple Deal With AT&T Falls Through`);
+test(convert, `this v that`, `This v That`);
+test(convert, `this vs that`, `This vs That`);
+test(convert, `this v. that`, `This v. That`);
+test(convert, `this vs. that`, `This vs. That`);
+test(convert, `The SEC's Apple probe: what you need to know`, `The SEC's Apple Probe: What You Need to Know`);
+test(convert, `'by the way, small word at the start but within quotes.'`, `'By the Way, Small Word at the Start but Within Quotes.'`);
+test(convert, `Small word at end is nothing to be afraid of`, `Small Word at End Is Nothing to Be Afraid Of`);
+test(convert, `Starting sub-phrase with a small word: a trick, perhaps?`, `Starting Sub-Phrase With a Small Word: A Trick, Perhaps?`);
+test(convert, `Sub-phrase with a small word in quotes: 'a trick, perhaps?'`, `Sub-Phrase With a Small Word in Quotes: 'A Trick, Perhaps?'`);
+test(convert, `Sub-phrase with a small word in quotes: "a trick, perhaps?"`, `Sub-Phrase With a Small Word in Quotes: "A Trick, Perhaps?"`);
+test(convert, `"Nothing to Be Afraid of?"`, `"Nothing to Be Afraid Of?"`);
+test(convert, `a thing`, `A Thing`);
+test(convert, `Dr. Strangelove (or: how I Learned to Stop Worrying and Love the Bomb)`, `Dr. Strangelove (Or: How I Learned to Stop Worrying and Love the Bomb)`);
+test(convert, `  this is trimming`, `This Is Trimming`);
+test(convert, `this is trimming  `, `This Is Trimming`);
+test(convert, `  this is trimming  `, `This Is Trimming`);
+test(convert, `IF IT’S ALL CAPS, FIX IT`, `If It’s All Caps, Fix It`);
+test(convert, `___if emphasized, keep that way___`, `___If Emphasized, Keep That Way___`);
+test(convert, `What could/should be done about slashes?`, `What Could/Should Be Done About Slashes?`);
+test(convert, `Never touch paths like /var/run before/after /boot`, `Never Touch Paths Like /var/run Before/After /boot`);
+
+test('exclusions', (t) => {
+	t.is(titleCase('Nothing to Be Afraid of?', {
+		excludedWords: ['be', 'of']
+	}), 'Nothing to be Afraid Of?');
+
+	t.is(titleCase('Nothing to Be Afraid of?', {
+		excludedWords: ['be', 'of'],
+		useDefaultExcludedWords: false
+	}), 'Nothing To be Afraid Of?');
+});
